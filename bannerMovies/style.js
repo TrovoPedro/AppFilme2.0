219cd7888b6d003adb4db19b97e8acbf675d484c
Pedro Trovo <98984115+TrovoPedro@users.noreply.github.com>
2023-05-26 17:33:26 -0300

    Add files via upload

---
+++ bannerMovies/style.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from "react-native"
+
+const styles = StyleSheet.create({
+
+    textBanner:{ 
+        fontSize:30,
+        color:"white",
+        marginTop:15,
+        marginLeft: -180,
+        fontWeight: 'Bold'
+    },
+
+    imageBanner:{
+        width: '50%',
+        height: 450,
+        marginTop:15,
+        borderRadius:10
+    }
+})
+
+export default styles;
